a47fed10ec3244594d5b66ae040e18b3ef3582bb
thunderstorm010 <yigitozdemir1299@gmail.com>
2020-08-05 01:46:06 +0300

    wdsfzdx

--- src/main/kotlin/com/thunderstorm010/lunarbotjda/Avatar.kt
+++ src/main/kotlin/com/thunderstorm010/lunarbotjda/Avatar.kt
@@ -13,41 +13,47 @@ class Avatar : ListenerAdapter() {
     private val logger: Logger = LoggerFactory.getLogger(Avatar::class.java)
     override fun onMessageReceived(event: MessageReceivedEvent) {
         System.setProperty(
-            "http.agent",
-            "HTC Mozilla/5.0 (Linux; Android 7.0; HTC 10 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.83 Mobile Safari/537.36"
+                "http.agent",
+                "HTC Mozilla/5.0 (Linux; Android 7.0; HTC 10 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.83 Mobile Safari/537.36"
         )
         if (event.isWebhookMessage) return
         if (event.author.isBot) return
         if (!event.message.contentRaw.startsWith("!avatar")) return
         if (event.message.mentionedUsers.size > 1) {
             val embed1 = EmbedBuilder()
-                .setAuthor(
-                    event.author.name + "#" + event.author.discriminator + ", ",
-                    null,
-                    event.author.effectiveAvatarUrl
-                )
-                .setDescription("Etiketlediğiniz kişi sayısı 1'den fazla olmamalıdır.")
-                .setTitle("!dmduyuru")
-                .setFooter(Main.footer_text, Main.footer_icon_url)
+                    .setAuthor(
+                            event.author.name + "#" + event.author.discriminator + ", ",
+                            null,
+                            event.author.effectiveAvatarUrl
+                    )
+                    .setDescription("Etiketlediğiniz kişi sayısı 1'den fazla olmamalıdır.")
+                    .setTitle("!dmduyuru")
+                    .setFooter(Main.footer_text, Main.footer_icon_url)
             event.message.channel.sendMessage("Etiketlediğiniz kişi sayısı 1 den fazla olmamalıdır.").queue()
         }
         if (event.message.mentionedUsers.size == 0) {
             EmbedBuilder()
-                .setTitle("İşte avatarın: ")
-                .setImage(event.author.effectiveAvatarUrl + "?size=4096")
-                .setColor(Color.BLACK)
-                .build().let {
-                    event.channel.sendMessage(it).queue()
-                }
+                    .setTitle("İşte avatarın: ")
+                    .setImage(event.author.effectiveAvatarUrl + "?size=4096")
+                    .setColor(Color.BLACK)
+                    .build().let {
+                        event.channel.sendMessage(it).queue()
+                    }
             logger.info(event.author.name + " doğru bir şekilde (kendi üzerine) avatar çağrısı yaptı.")
         } else if (event.message.mentionedUsers.size in 1 downTo 0) {
             EmbedBuilder()
-                .setTitle("İşte ${event.message.mentionedUsers[0].name} kullanıcısının avatarı: ")
-                .setImage(event.message.mentionedUsers[0].effectiveAvatarUrl + "?size=4096")
-                .setColor(Color.BLACK)
-                .build().let {
-                    event.channel.sendMessage(it).queue()
-                }
+                    .setAuthor(
+                            event.author.name + "#" + event.author.discriminator + ", ",
+                            null,
+                            event.author.effectiveAvatarUrl
+                    )
+                    .setTitle("İşte ${event.message.mentionedUsers[0].name} kullanıcısının avatarı: ")
+                    .setImage(event.message.mentionedUsers[0].effectiveAvatarUrl + "?size=4096")
+                    .setColor(Color.BLACK)
+                    .setFooter(Main.footer_text,Main.footer_icon_url)
+                    .build().let {
+                        event.channel.sendMessage(it).queue()
+                    }
             logger.info(event.author.name + ", " + event.message.mentionedUsers[0].name + " üzerine doğru bir şekilde avatar çağrısı yaptı.")
         }
 
