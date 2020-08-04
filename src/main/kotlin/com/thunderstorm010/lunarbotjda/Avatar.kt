5ce5a89649bc6e775f5ec93e7ad6252cd7193ae7
thunderstorm010 <yigitozdemir1299@gmail.com>
2020-08-04 21:28:08 +0300

    H

--- src/main/kotlin/com/thunderstorm010/lunarbotjda/Avatar.kt
+++ src/main/kotlin/com/thunderstorm010/lunarbotjda/Avatar.kt
@@ -1,7 +1,7 @@
 package com.thunderstorm010.lunarbotjda
 
 import Main
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter
+
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
